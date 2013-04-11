0881d57755da157b9059eb5b0d01bd3ceee2c186
Scott Tepsuporn <sc.teps92@gmail.com>
2013-04-11 14:43:31 -0400

    Finish implementing all poisson agents

--- src/edu/virginia/jinsup/HFTPoisson.java
+++ src/edu/virginia/jinsup/HFTPoisson.java
@@ -4,13 +4,83 @@ public class HFTPoisson extends PoissonAgent {
 
   public HFTPoisson(MatchingEngine matchEng, int lambdaOrder, int lambdaCancel) {
     super(matchEng, lambdaOrder, lambdaCancel);
-    // TODO Auto-generated constructor stub
   }
 
   @Override
   void makeOrder() {
-    // TODO Auto-generated method stub
-
+    double factor =
+      getBestBuyPrice() / (getBestBuyPrice() + getBestSellPrice());
+    boolean willBuy = true;
+    if (getInventory() > 19) {
+      // cancel all buy orders orders
+      cancelAllBuyOrders();
+      willBuy = false;
+    }
+    if (getInventory() < -19) {
+      // cancel all sell orders
+      cancelAllSellOrders();
+      willBuy = true;
+    }
+    if (factor < 0.10) {
+      willBuy = (Math.random() < 0.1);
+    }
+    else if (factor < 0.20) {
+      willBuy = (Math.random() < 0.2);
+    }
+    else if (factor < 0.30) {
+      willBuy = (Math.random() < 0.3);
+    }
+    else if (factor < 0.40) {
+      willBuy = (Math.random() < 0.4);
+    }
+    else if (factor < 0.50) {
+      willBuy = (Math.random() < 0.5);
+    }
+    else if (factor < 0.60) {
+      willBuy = (Math.random() < 0.6);
+    }
+    else if (factor < 0.70) {
+      willBuy = (Math.random() < 0.7);
+    }
+    else if (factor < 0.80) {
+      willBuy = (Math.random() < 0.8);
+    }
+    else if (factor < 0.90) {
+      willBuy = (Math.random() < 0.9);
+    }
+    // calculate the probability
+    double probability = Math.random();
+    // Market order 2% of the time
+    if (probability < 0.02) {
+      // Issue market order, assuming only one market order is
+      createMarketOrder(1, willBuy);
+    }
+    // 1 tick of last trade price 15% of the time
+    else if (probability < 0.17) {
+      createNewOrder(getLastTradePrice(), 1, willBuy);
+    }
+    // 2 tick of last trade price 20% of the time
+    else if (probability < 0.37) {
+      createNewOrder(getLastTradePrice(), 2, willBuy);
+    }
+    // 3 tick of last trade price 15% of the time
+    else if (probability < 0.52) {
+      createNewOrder(getLastTradePrice(), 3, willBuy);
+    }
+    // 4 tick of last trade price 15% of the time
+    else if (probability < 0.67) {
+      createNewOrder(getLastTradePrice(), 4, willBuy);
+    }
+    // 5 tick of last trade price 15% of the time
+    else if (probability < 0.82) {
+      createNewOrder(getLastTradePrice(), 5, willBuy);
+    }
+    // 6 tick of last trade price 13% of the time
+    else if (probability < 0.95) {
+      createNewOrder(getLastTradePrice(), 6, willBuy);
+    }
+    else {
+      createNewOrder(getLastTradePrice(), 7, willBuy);
+    }
   }
-
 }
--- src/edu/virginia/jinsup/OpporStratPoisson.java
+++ src/edu/virginia/jinsup/OpporStratPoisson.java
@@ -1,14 +1,69 @@
 package edu.virginia.jinsup;
 
+import org.apache.commons.math3.distribution.UniformRealDistribution;
+
 public class OpporStratPoisson extends PoissonAgent {
 
-  public OpporStratPoisson(MatchingEngine matchEng, int lambdaOrder, int lambdaCancel) {
+  private double currBuyProbability = 0.0;
+
+  public OpporStratPoisson(MatchingEngine matchEng, int lambdaOrder,
+    int lambdaCancel, double initialBuyProbability) {
     super(matchEng, lambdaOrder, lambdaCancel);
+    currBuyProbability = initialBuyProbability;
   }
 
-  public void makeOrder()
-  {
-    
+  public void makeOrder() {
+    boolean willBuy = (Math.random() < currBuyProbability);
+    // calculate the probability for the next trade
+    currBuyProbability =
+      currBuyProbability
+        + (new UniformRealDistribution(0.2 - currBuyProbability,
+          currBuyProbability - 0.8)).sample();
+    double probability = Math.random();
+    // Market order 35% of the time
+    if (probability < 0.35) {
+      // Issue market order, assuming only one market order is
+      createMarketOrder(1, willBuy);
+    }
+    // 1 tick of last trade price 20% of the time
+    else if (probability < 0.55) {
+      createNewOrder(getLastTradePrice(), 1, willBuy);
+    }
+    // 2 tick of last trade price 5% of the time
+    else if (probability < 0.60) {
+      createNewOrder(getLastTradePrice(), 2, willBuy);
+    }
+    // 3 tick of last trade price 5% of the time
+    else if (probability < 0.65) {
+      createNewOrder(getLastTradePrice(), 3, willBuy);
+    }
+    // 4 tick of last trade price 5% of the time
+    else if (probability < 0.70) {
+      createNewOrder(getLastTradePrice(), 4, willBuy);
+    }
+    // 5 tick of last trade price 5% of the time
+    else if (probability < 0.75) {
+      createNewOrder(getLastTradePrice(), 5, willBuy);
+    }
+    // 6 tick of last trade price 7% of the time
+    else if (probability < 0.80) {
+      createNewOrder(getLastTradePrice(), 6, willBuy);
+    }
+    // 7 tick of last trade price 5% of the time
+    else if (probability < 0.85) {
+      createNewOrder(getLastTradePrice(), 7, willBuy);
+    }
+    // 8 tick of last trade price 5% of the time
+    else if (probability < 0.90) {
+      createNewOrder(getLastTradePrice(), 8, willBuy);
+    }
+    // 9 tick of last trade price 6% of the time
+    else if (probability < 0.96) {
+      createNewOrder(getLastTradePrice(), 9, willBuy);
+    }
+    // 10 tick of last trade price 4% of the time
+    else {
+      createNewOrder(getLastTradePrice(), 10, willBuy);
+    }
   }
-
 }
