845fa2b6f7647852b98d1b513b6f4c79a9a5658d
Scott Tepsuporn <sc.teps92@gmail.com>
2013-06-13 21:04:31 -0400

    Move hysteresis algorithm from ST to OS

--- src/edu/virginia/jinsup/OpporStratPoisson.java
+++ src/edu/virginia/jinsup/OpporStratPoisson.java
@@ -9,6 +9,17 @@ import org.apache.commons.math3.distribution.UniformRealDistribution;
 public class OpporStratPoisson extends PoissonAgent {
 
   /**
+   * Limits the number of shares owned by the agent.
+   */
+  private static final int INVENTORY_LIMIT = 5;
+
+  /**
+   * Whether or not agent owns more shares than INVENTORY_LIMIT or has a deficit
+   * of more than -INVENTORY_LIMIT.
+   */
+  private boolean overLimit;
+
+  /**
    * Global buy probability for all poisson opportunistic traders
    */
   private static double currBuyProbability = 0.0;
@@ -31,10 +42,33 @@ public class OpporStratPoisson extends PoissonAgent {
     int lambdaCancel, double initialBuyProbability, long initialActTime) {
     super(matchEng, lambdaOrder, lambdaCancel, initialActTime);
     currBuyProbability = initialBuyProbability;
+    overLimit = false;
   }
 
   public void makeOrder() {
-    boolean willBuy = (Math.random() < currBuyProbability);
+    boolean willBuy = true;
+    // Whether or not to skip factor checking
+    boolean override = true;
+
+    if (getInventory() > INVENTORY_LIMIT) {
+      overLimit = true;
+      willBuy = false;
+      cancelAllBuyOrders();
+    } else if (getInventory() < -INVENTORY_LIMIT) {
+      overLimit = true;
+      willBuy = true;
+      cancelAllSellOrders();
+    } else if (getInventory() > INVENTORY_LIMIT / 2 && overLimit) {
+      willBuy = false;
+    } else if (getInventory() < -INVENTORY_LIMIT / 2 && overLimit) {
+      willBuy = true;
+    } else if (getInventory() < Math.abs(INVENTORY_LIMIT) / 2) {
+      overLimit = false;
+      override = false;
+    }
+    if (!override) {
+      willBuy = (Math.random() < currBuyProbability);
+    }
     createPoissonOrder(willBuy,
       getOrderSize(0.66, 0.16, 0.05, 0.04, 0.03, 0.03, 0.03), 0.35, 0.20, 0.05,
       0.05, 0.05, 0.05, 0.07, 0.05, 0.05, 0.06, 0.04);
--- src/edu/virginia/jinsup/SmallTrader.java
+++ src/edu/virginia/jinsup/SmallTrader.java
@@ -7,16 +7,7 @@ import java.util.Random;
  */
 public class SmallTrader extends PoissonAgent {
 
-  /**
-   * Limits the number of shares owned by the agent.
-   */
-  private static final int INVENTORY_LIMIT = 10;
-
-  /**
-   * Whether or not agent owns more shares than INVENTORY_LIMIT or has a deficit
-   * of more than -INVENTORY_LIMIT.
-   */
-  private boolean overLimit;
+  private Random rand;
 
   /**
    * Creates a small trader.
@@ -33,33 +24,12 @@ public class SmallTrader extends PoissonAgent {
   public SmallTrader(MatchingEngine matchEng, int lambdaOrder,
     int lambdaCancel, long initialActTime) {
     super(matchEng, lambdaOrder, lambdaCancel, initialActTime);
-    overLimit = false;
+    rand = new Random();
   }
 
   @Override
   void makeOrder() {
-    boolean willBuy = true;
-
-    // Whether or not to skip factor checking
-    boolean override = true;
-
-    if (getInventory() > INVENTORY_LIMIT) {
-      overLimit = true;
-      willBuy = false;
-      cancelAllBuyOrders();
-    } else if (getInventory() < -INVENTORY_LIMIT) {
-      overLimit = true;
-      willBuy = true;
-      cancelAllSellOrders();
-    } else if (getInventory() > INVENTORY_LIMIT / 2 && overLimit) {
-      willBuy = false;
-    } else if (getInventory() < -INVENTORY_LIMIT / 2 && overLimit) {
-      willBuy = true;
-    } else if (getInventory() < Math.abs(INVENTORY_LIMIT) / 2) {
-      overLimit = false;
-      override = false;
-    }
-
+    boolean willBuy = rand.nextBoolean();
     createPoissonOrder(willBuy, getOrderSize(0.98, 0.02), 0.20, 0.11, 0.09,
       0.07, 0.07, 0.07, 0.07, 0.05, 0.05, 0.10, 0.12);
   }
