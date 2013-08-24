7b90df9ed0cc5fc1f2dbf35085601f03c800247b
Art Chaidarun <artchaidarun@gmail.com>
2013-08-23 23:50:44 -0400

    Remove batch mode

--- src/edu/virginia/jinsup/JinSup.java
+++ src/edu/virginia/jinsup/JinSup.java
@@ -1,13 +1,8 @@
 package edu.virginia.jinsup;
 
-import java.awt.Frame;
-
 import com.beust.jcommander.JCommander;
 
 public class JinSup {
-
-  public static final boolean BATCH_MODE = true;
-  public static final int NUMBER_OF_RUNS = 2;
 
   /**
    * If true, no trade logging information will be saved and graphs will not be
@@ -28,30 +23,11 @@ public class JinSup {
     MatchingEngine matchingEngine;
     Controller controller;
 
-    if (!BATCH_MODE) {
-      matchingEngine = new MatchingEngine(buyPrice, startTime, TEST_MODE);
-      controller =
-        new Controller(startTime, endTime, matchingEngine, TEST_MODE);
-      System.out.println("Starting simulator...");
-      long elapsedTime = System.nanoTime();
-      controller.runSimulator();
-      Controller.graphFrame.showFinished(System.nanoTime() - elapsedTime);
-    } else {
-      Controller.graphFrame.dispose();
-      for (int i = 0; i < NUMBER_OF_RUNS; i++) {
-        Controller.graphFrame = new GraphFrame();
-        Controller.graphFrame.setState(Frame.ICONIFIED);
-        matchingEngine = new MatchingEngine(buyPrice, startTime, TEST_MODE);
-
-        controller =
-          (Settings.isSet() ? new Controller(startTime, endTime,
-            matchingEngine, TEST_MODE, Settings.threshold, Settings.delay)
-            : new Controller(startTime, endTime, matchingEngine, TEST_MODE));
-        System.out.println("Starting simulator, run " + (i + 1) + " of "
-          + NUMBER_OF_RUNS + " ...");
-        controller.runSimulator();
-        Controller.graphFrame.dispose();
-      }
-    }
+    matchingEngine = new MatchingEngine(buyPrice, startTime, TEST_MODE);
+    controller = new Controller(startTime, endTime, matchingEngine, TEST_MODE);
+    System.out.println("Starting simulator...");
+    long elapsedTime = System.nanoTime();
+    controller.runSimulator();
+    Controller.graphFrame.showFinished(System.nanoTime() - elapsedTime);
   }
 }
